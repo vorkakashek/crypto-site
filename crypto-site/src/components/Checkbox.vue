4f6725ab13bb235e12053da3734a19fb2d10e0b8
Vyacheslav <vyacheslavlutsenkoml@gmail.com>
2023-03-06 16:12:31 +0500

    feat: added login, registration, restore password

--- crypto-site/src/components/Checkbox.vue
+++ crypto-site/src/components/Checkbox.vue
@@ -1,99 +1,103 @@
 <script setup>
+import { ref, computed } from "vue";
+import { useModal } from "@/stores/store";
 
-import { ref, computed } from 'vue'
-import { useModal } from "@/stores/store"
-
-const store = useModal()
+const store = useModal();
 
 let props = defineProps({
-    text: {
-        type: String,
-        required: false,
-        default: '',
-    },
-    required: {
-        type: Boolean,
-        required: false,
-        default: true,
-    }
-})
+  text: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  required: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  id: {
+    type: String,
+    required: false,
+  },
+});
 
-let checkbox = ref(false)
-
+let checkbox = ref(false);
 </script>
 
 <template>
-    <label class="custom_input">
-        <div :class="[{ checked: checkbox }, 'checkbox']">
-            <input type="checkbox" v-model="checkbox" :required="required">
-        </div>
-        <i18n-t keypath="modal.terms" tag="p" scope="global">
-            <RouterLink to="/terms" @click="store.hide()">{{ $t('modal.tos') }}</RouterLink>
-        </i18n-t>
-    </label>
+  <label class="custom_input">
+    <div :class="[{ checked: checkbox }, 'checkbox']">
+      <input type="checkbox" v-model="checkbox" :required="required" :id="id" />
+    </div>
+    <i18n-t keypath="modal.terms" tag="p" scope="global">
+      <RouterLink to="/terms" @click="store.hide()">{{
+        $t("modal.tos")
+      }}</RouterLink>
+    </i18n-t>
+  </label>
 </template>
 
 <style lang="scss" scoped>
 .custom_input {
-    display: flex;
-    align-items: flex-start;
+  display: flex;
+  align-items: flex-start;
 
-    p {
-        margin-top: 0;
-        margin-left: 12px;
-        font-size: 12px;
-        letter-spacing: .02rem;
-        line-height: 1.6;
-    }
+  p {
+    margin-top: 0;
+    margin-left: 12px;
+    font-size: 12px;
+    letter-spacing: 0.02rem;
+    line-height: 1.6;
+  }
 
-    a {
-        color: var(--blue);
-        font-weight: 700;
-        margin: 0;
-        font-size: 12px;
-        letter-spacing: .02rem;
-        line-height: 1.6;
-    }
+  a {
+    color: var(--blue);
+    font-weight: 700;
+    margin: 0;
+    font-size: 12px;
+    letter-spacing: 0.02rem;
+    line-height: 1.6;
+  }
 
-    input {
-        opacity: 0;
-        user-select: none;
-        pointer-events: none;
-    }
+  input {
+    opacity: 0;
+    user-select: none;
+    pointer-events: none;
+  }
 }
 
 .checkbox {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 18px;
-    height: 18px;
-    cursor: pointer;
-    border-radius: 4px;
-    border: 1px solid var(--light);
-    flex-shrink: 0;
-    position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 1px solid var(--light);
+  flex-shrink: 0;
+  position: relative;
 
-    &.checked {
-        border-color: var(--blue);
-
-        &:after {
-            opacity: 1;
-            transform: scale(1);
-        }
-    }
+  &.checked {
+    border-color: var(--blue);
 
     &:after {
-        content: '';
-        position: absolute;
-        display: block;
-        width: 14px;
-        height: 14px;
-        border-radius: 4px;
-        background-color: var(--blue);
-        opacity: 0;
-        transform: scale(0);
-        transition: all .15s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+      opacity: 1;
+      transform: scale(1);
     }
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 14px;
+    height: 14px;
+    border-radius: 4px;
+    background-color: var(--blue);
+    opacity: 0;
+    transform: scale(0);
+    transition: all 0.15s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  }
 }
 </style>
--- crypto-site/src/components/Modal/ModalLogin.vue
+++ crypto-site/src/components/Modal/ModalLogin.vue
@@ -1,169 +1,298 @@
 <script setup>
-import { ref, watch, computed } from 'vue'
-import { useModal } from "@/stores/store"
-import { useUser } from "@/stores/store"
+import { ref, watch, computed } from "vue";
+import { useModal } from "@/stores/store";
+import { useUser } from "@/stores/store";
+import axios from "redaxios";
 
-import Panel from "@/components/Panel.vue"
-import Input from "@/components/Input.vue"
-import Checkbox from "@/components/Checkbox.vue"
-import Button from "@/components/Button.vue"
+import Panel from "@/components/Panel.vue";
+import Input from "@/components/Input.vue";
+import Checkbox from "@/components/Checkbox.vue";
+import Button from "@/components/Button.vue";
 
-
-let store = useModal()
-let storeUser = useUser()
+let store = useModal();
+let storeUser = useUser();
 
 let tab_current = computed({
-    get: () => store.currentTab,
-    set: (val) => {
-        store.currentTab = val
-        return val
-    }
-})
+  get: () => store.currentTab,
+  set: (val) => {
+    store.currentTab = val;
+    return val;
+  },
+});
 
 let form_error = ref(false),
-    timer = ref(),
-    restore_tab = ref(false)
+  timer = ref(),
+  restore_tab = ref(false),
+  restore_tab_2 = ref(false);
 
 let sign_in = (e) => {
-    // form_error.value = true
-    storeUser.setLoggedIn(true)
-    store.hide()
-}
+  let email = document.getElementById("email_sign_in");
+  let password = document.getElementById("password_sign_in");
+
+  if (email.checkValidity() && password.checkValidity()) {
+    axios
+      .post("https://fatpockets.io/api/v1/login", {
+        email: email.value,
+        password: password.value,
+      })
+      .then((res) => {
+        if (res.data.success) {
+          storeUser.setLoggedIn(true);
+          localStorage.setItem("UUID_V4", res.data.user.uuid);
+          store.hide();
+        }
+      })
+      .catch((err) => {
+        alert("Пользователь не найден или произошла ошибка");
+      });
+  } else {
+    form_error.value = true;
+  }
+};
 
 let sign_up = (e) => {
-    form_error.value = true
-}
+  let email = document.getElementById("email_sign_up");
+  let password = document.getElementById("password_sign_up");
+  let check = document.getElementById("check_sign_up");
+  if (
+    email.checkValidity() &&
+    password.checkValidity() &&
+    check.checkValidity()
+  ) {
+    axios
+      .post("https://fatpockets.io/api/v1/signup", {
+        email: email.value,
+        password: password.value,
+      })
+      .then((res) => {
+        if (res.data.success) {
+          storeUser.setLoggedIn(true);
+          localStorage.setItem("UUID_V4", res.data.user.uuid);
+          store.hide();
+        }
+      })
+      .catch((err) => {
+        alert("Регистрация не прошла, попробуйте позже");
+      });
+  } else {
+    form_error.value = true;
+  }
+};
 
 let restore = (e) => {
-    form_error.value = true
-}
+  let email = document.getElementById("email_restore");
+  if (email.checkValidity()) {
+    axios
+      .post("https://fatpockets.io/api/v1/forget", {
+        email: email.value,
+      })
+      .then((res) => {
+        restore_tab.value = false;
+        restore_tab_2.value = true;
+        console.log(res.data);
+      })
+      .catch((err) => {
+        alert("Пользователь не найден");
+      });
+  } else {
+    form_error.value = true;
+  }
+};
+
+let new_password = (e) => {
+  let code = document.getElementById("code_restore");
+  let password = document.getElementById("password_restore");
+  if (code.checkValidity() && password.checkValidity()) {
+    axios
+      .post(`https://fatpockets.io/api/v1/restore/${code.value}`, {
+        password: password.value,
+      })
+      .then((res) => {
+        if (res.data.success) {
+          alert(res.data.message);
+          storeUser.setLoggedIn(true);
+          localStorage.setItem("UUID_V4", res.data.user.uuid);
+          store.hide();
+        }
+      });
+    // .catch((err) => {
+    //   alert("Произошла ошибка");
+    // });
+  } else {
+    form_error.value = true;
+  }
+};
 
 watch(form_error, () => {
-    if (form_error.value) {
-        timer.value = setTimeout(() => {
-            form_error.value = false
-        }, 5000)
-    }
-})
+  if (form_error.value) {
+    timer.value = setTimeout(() => {
+      form_error.value = false;
+    }, 5000);
+  }
+});
 
-watch(tab_current, () => form_error.value = false)
-
+watch(tab_current, () => (form_error.value = false));
 </script>
 
 <template>
-    <Panel>
-        <template #content>
-            <div class="tab_group">
-                <div class="tab_list">
-                    <label :class="[{ active: index === tab_current }, 'tab_item']"
-                        v-for="(tab_name, index) in $tm('modal.tab_names')">
-                        <input type="radio" :value="index" v-model="tab_current">
-                        {{ tab_name }}
-                    </label>
-                </div>
-                <div class="tab_content" v-if="tab_current === 0 && !restore_tab">
-                    <form>
-                        <Input placeholder="e-mail" type="email" />
-                        <Input placeholder="password" type="password" />
-                        <div class="restore_pass" @click="restore_tab = true">{{ $t("modal.restore_msg") }}</div>
-                        <Button :text="$t('main.sign_in')" @click="sign_in" />
-                        <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
-                    </form>
-                </div>
-                <div class="tab_content" v-if="tab_current === 1">
-                    <form>
-                        <Input placeholder="e-mail" type="email" />
-                        <Input placeholder="password" type="password" />
-                        <Checkbox :text="$t('modal.terms')" />
-                        <Button :text="$t('main.sign_up')" @click="sign_up" />
-                        <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
-                    </form>
-                </div>
-                <div class="tab_content" v-if="tab_current === 0 && restore_tab">
-                    <form>
-                        <Input placeholder="e-mail" type="email" />
-                        <Button :text="$t('main.restore')" @click="restore" />
-                        <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
-                        <div class="restore_pass_back" @click="restore_tab = false">{{ $t("modal.back") }}</div>
-                    </form>
-                </div>
+  <Panel>
+    <template #content>
+      <div class="tab_group">
+        <div class="tab_list">
+          <label
+            :class="[{ active: index === tab_current }, 'tab_item']"
+            v-for="(tab_name, index) in $tm('modal.tab_names')"
+          >
+            <input type="radio" :value="index" v-model="tab_current" />
+            {{ tab_name }}
+          </label>
+        </div>
+        <div
+          class="tab_content"
+          v-if="tab_current === 0 && !restore_tab && !restore_tab_2"
+        >
+          <form>
+            <Input placeholder="e-mail" type="email" id="email_sign_in" />
+            <Input
+              minlength="6"
+              placeholder="password"
+              type="password"
+              id="password_sign_in"
+            />
+            <div class="restore_pass" @click="restore_tab = true">
+              {{ $t("modal.restore_msg") }}
             </div>
-        </template>
-    </Panel>
+            <Button :text="$t('main.sign_in')" @click="sign_in" />
+            <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
+          </form>
+        </div>
+
+        <div class="tab_content" v-if="tab_current === 1">
+          <form>
+            <Input placeholder="e-mail" type="email" id="email_sign_up" />
+            <Input
+              minlength="6"
+              placeholder="password"
+              type="password"
+              id="password_sign_up"
+            />
+            <Checkbox :text="$t('modal.terms')" id="check_sign_up" />
+            <Button :text="$t('main.sign_up')" @click="sign_up" />
+            <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
+          </form>
+        </div>
+
+        <div class="tab_content" v-if="tab_current === 0 && restore_tab">
+          <form>
+            <Input placeholder="e-mail" type="email" id="email_restore" />
+            <Button :text="$t('main.restore')" @click="restore" />
+            <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
+            <div class="restore_pass_back" @click="restore_tab = false">
+              {{ $t("modal.back") }}
+            </div>
+          </form>
+        </div>
+
+        <div class="tab_content" v-if="tab_current === 0 && restore_tab_2">
+          <form>
+            <Input placeholder="code" id="code_restore" />
+            <Input
+              minlength="6"
+              placeholder="password"
+              type="password"
+              id="password_restore"
+            />
+            <Button :text="$t('main.restore')" @click="new_password" />
+            <div id="form_error" v-if="form_error">{{ $t("modal.error") }}</div>
+            <div
+              class="restore_pass_back"
+              @click="
+                () => {
+                  restore_tab_2 = false;
+                  restore_tab = true;
+                }
+              "
+            >
+              {{ $t("modal.back") }}
+            </div>
+          </form>
+        </div>
+      </div>
+    </template>
+  </Panel>
 </template>
 
 <style lang="scss" scoped>
 .restore_pass,
 .restore_pass_back {
-    cursor: pointer;
-    font-weight: 700;
-    color: #70748D;
-    font-size: 12px;
-    letter-spacing: .02rem;
-    display: inline-block;
-    margin-bottom: 16px;
+  cursor: pointer;
+  font-weight: 700;
+  color: #70748d;
+  font-size: 12px;
+  letter-spacing: 0.02rem;
+  display: inline-block;
+  margin-bottom: 16px;
 
-    &:hover {
-        color: var(--blue);
-    }
+  &:hover {
+    color: var(--blue);
+  }
 }
 
 .restore_pass_back {
-    text-align: center;
-    display: block;
-    margin-top: 24px;
-    margin-bottom: 0;
+  text-align: center;
+  display: block;
+  margin-top: 24px;
+  margin-bottom: 0;
 }
 
 #form_error {
-    color: var(--error);
-    font-weight: 700;
-    text-align: center;
-    letter-spacing: .1rem;
-    text-transform: uppercase;
-    font-size: 11px;
-    opacity: .8;
+  color: var(--error);
+  font-weight: 700;
+  text-align: center;
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+  font-size: 11px;
+  opacity: 0.8;
 }
 
 .tab_content {
-    margin-top: 32px;
+  margin-top: 32px;
 }
 
 .tab_list {
-    display: flex;
-    border-radius: 16px;
-    border: 1px solid #E8E8E8;
-    overflow: hidden;
-    margin-bottom: 16px;
+  display: flex;
+  border-radius: 16px;
+  border: 1px solid #e8e8e8;
+  overflow: hidden;
+  margin-bottom: 16px;
 }
 
 .tab_item {
-    flex: 1 0;
-    padding: 16px;
-    text-align: center;
-    font-size: 14px;
-    font-weight: 700;
-    color: #70748D;
-    opacity: .7;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
+  flex: 1 0;
+  padding: 16px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 700;
+  color: #70748d;
+  opacity: 0.7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 
-    input {
-        display: none;
-    }
+  input {
+    display: none;
+  }
 
-    &.active {
-        background-color: var(--blue);
-        color: #fff;
-        opacity: 1;
-    }
+  &.active {
+    background-color: var(--blue);
+    color: #fff;
+    opacity: 1;
+  }
 
-    @include r (md) {
-        font-size: 12px;
-        padding: 8px 12px;
-    }
-
+  @include r(md) {
+    font-size: 12px;
+    padding: 8px 12px;
+  }
 }
 </style>
